d96c096065f68d26302e7835675876e3a17cb9b3
Hector Aristy <rafelito266@gmail.com>
2018-03-18 14:34:09 -0400

    move test to home container.

--- movie/src/containers/home/index.test.js
+++ movie/src/containers/home/index.test.js
@@ -1,6 +1,6 @@
 import 'react-native';
 import React from 'react';
-import Home from '../src/containers/Home';
+import Home from './index';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
